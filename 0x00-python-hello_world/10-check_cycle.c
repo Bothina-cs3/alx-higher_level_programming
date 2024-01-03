a5f6b8b334fe34548f1ca72a0a4ae7b345c83cb3
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-01-02 18:19:47 -0800

    ss

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list"
+/**
+ * check_cycle - chech if list is cyclical
+ * @list: pointer to list
+ * Return: 1 if cyclical, 0 otherwise
+*/
+int check_cycle(listint_t *list)
+{
+	istint_t *slow = list, *fast = list;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
