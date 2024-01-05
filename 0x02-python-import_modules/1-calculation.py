81750ffb4331f44818bb7664d6ea1ee8a5ffe7b0
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-01-04 16:37:25 -0800

    add

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,12 +4,13 @@ if __name__ == "__main__":
     a = 10 
     b = 5
 
-     result_add = add(a, b)
+    result_add = add(a, b)
     result_sub = sub(a, b)
     result_mul = mul(a, b)
     result_div = div(a, b)
 
-    print("{} + {} = {}".format(a, b result_add(a, b)))
-    print("{} - {} = {}".format(a, b result_sub(a, b)))
-    print("{} * {} = {}".format(a, b result_mul(a, b)))
-    print("{} / {} = {}".format(a, b result_mul(a, b)))
+    print(f"{a} + {b} = {result_add}")
+    print(f"{a} - {b} = {result_sub}")
+    print(f"{a} * {b} = {result_mul}")
+    print(f"{a} / {b} = {result_div}")
+
