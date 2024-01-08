1d80f5248f6904c6d04fba4200a6317919cb502b
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-01-08 05:08:17 -0800

    add

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return None
-
-    listdivs = []
+    new_list = []
     for i in my_list:
-        if i % 2 == 0:
-            listdivs.append(True)
+        if (i % 2) == 0:
+            new_list.append(True)
         else:
-            listdivs.append(False)
-        return listdivs
+            new_list.append(False)
+        return (new_list)
