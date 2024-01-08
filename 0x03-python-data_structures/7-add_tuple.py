0c8fdb9b9fc2b4cdc8ddd470ea1a1b3269f35a34
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-01-08 04:35:48 -0800

    add

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1 = typle_a[0] if len(tuple_a) > 0 else 0
-    a2 = typle_a[1] if len(tuple_a) > 1 else 0
-    b1 = typle_b[0] if len(tuple_b) > 0 else 0
-    b2 = typle_b[1] if len(tuple_b) > 1 else 0
-    return (a1 + b1, a2 +b2)
+    a1, a2 = tuple_a if len(tuple_a) >= 2 else (0, 0)
+    b1, b2 = tuple_b if len(tuple_b) >= 2 else (0, 0)
+    return (a1 + b1, a2 + b2)
