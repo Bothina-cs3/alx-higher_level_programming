1d3ebc1e9608ab2803b5470a51219d2a9f4892c6
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-01-22 13:08:07 +0000

    add

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,11 @@
 def safe_print_list(my_list=[], x=0):
 	i = 0
 	try:
-		while i is not x:
-			print(my_list[i], end='')
+	    while i is not x:
+		print(my_list[i], end='')
 			i += 1
 	except IndexError:
-		None
+	    None
 	print()
 	return i
+
