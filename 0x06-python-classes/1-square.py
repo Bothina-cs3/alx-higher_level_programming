91c91b81048ec0e19cf5bbf98debe2451994c3c6
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-01-23 08:13:28 -0800

    add

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Defines a class Square"""
+
+def __init__(self, size):
+    """Constructor.
+
+    Args:
+        size: size of the Square
+    """
+    self.__size = size
