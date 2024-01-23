ee8512b64780cda0c0d06de7d82c2235990ab3c7
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-01-23 09:58:35 -0800

    add

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Defines a Square."""
+
+    def __init__(self, size=0):
+        """constructor.
+
+        Args:
+            size: size of the Square
+            """
+            self.__size = size
+
+    def area(self):
+        """Calulates the area of Square.
+
+        Returns: the current Square area.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Returns: the size of a Square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter for size.
+
+        Args:
+            value (int): size of a Square
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
