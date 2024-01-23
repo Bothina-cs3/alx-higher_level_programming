731c2b20fb7dcac884d5e7ece82f6eaf631c6846
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-01-23 09:04:56 -0800

    add

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,10 +13,9 @@ class Square:
             """
             self.__size = size
 
-            if not isinstance(size, init):
+            if not isinstance(size, int):
                 raise TypeError("size must be an integer")
-            elif size < 0:
+            if size < 0:
                 raise ValueError("size must be >= 0")
-            else:
                 self.__size = size
 
