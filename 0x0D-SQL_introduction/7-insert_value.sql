1b1a84b18bba675e13f23f7876b9e1b7d769c049
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-03-13 04:03:30 +0300

    add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- This script inserts a new row into `first_table`
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+
