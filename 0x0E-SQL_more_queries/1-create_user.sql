079c0b6edfab164ab00db27625ab13c05cc5425f
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-03-14 03:32:48 +0300

    add

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,12 +1,9 @@
 -- Write a script that creates the MySQL server user user_0d_1.
--- The user_0d_1 password should be set to user_0d_1_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
