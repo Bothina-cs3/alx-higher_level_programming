0c54ed81d6101d4de42bbf1b7b8dfca9559fcda0
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-03-14 03:52:09 +0300

    add

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- 8-cities_of_california_subquery.sql
+-- Lists all the cities of California without using JOIN keyword
+
+SELECT * FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id;
