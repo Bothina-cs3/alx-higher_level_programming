72af781658088f5e44b453343b8d91f21e932cf0
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-03-14 03:46:31 +0300

    add

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- 5-unique_id.sql
+-- Creates table unique_id with id defaulting to 1 and must be unique
+
+CREATE TABLE IF NOT EXISTS unique_id (
+  id INT DEFAULT 1 UNIQUE,
+  name VARCHAR(256)
+);
