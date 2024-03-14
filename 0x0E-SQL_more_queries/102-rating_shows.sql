1dbcd8567a4280945166f6f6631bf75b92097006
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-03-14 04:15:04 +0300

    add

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,9 @@
--- 102-rating_shows.sql
--- Lists all shows from hbtn_0d_tvshows_rate by their rating
-
-SELECT tv_shows.title, SUM(ratings.rating) AS rating_sum
-    FROM tv_shows
-    JOIN ratings ON tv_shows.id = ratings.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Records are ordered by descending rating.
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
+ 
