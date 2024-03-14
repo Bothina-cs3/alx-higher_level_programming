cf45146bf25662320851fb23d977ec51ffa55f46
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-03-14 04:06:03 +0300

    add

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,10 @@
--- 8-cities_of_california_subquery.sql
--- Lists all the cities of California without using JOIN keyword
-SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+-- lists all the cities of California in the database hbtn_0d_usa
+   -- states table contains only one record where name = California
+   -- Results must be sorted in ascending order by cities.id
+   -- The database name will be passed as an argument of the mysql command
+
+SELECT id, name FROM cities
+WHERE state_id = (
+      SELECT id FROM states
+      WHERE name = "California")
 ORDER BY id;
