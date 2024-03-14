bc239a8ad74a8f520b64588295efd681c0cd6d6b
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-03-14 04:17:26 +0300

    add

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,11 +1,9 @@
--- 1-create_user.sql
--- Create user_0d_1 with all privileges and a specific password. Do not fail if the user already exists.
-
--- Attempt to grant all privileges to the user, which will create the user if it doesn't exist
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' WITH GRANT OPTION;
-
--- Ensure the password is set for the user (redundant if the user was just created above, but necessary if the user already existed)
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Apply changes immediately
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
