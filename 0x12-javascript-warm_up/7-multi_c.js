438a1c643ffbef936fa3b17ce56f97af5ca95dd9
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-04-09 04:48:15 +0300

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const numOccurrences = parseInt(process.argv[2]);
-
-if (isNaN(numOccurrences) || numOccurrences < 1) {
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < numOccurrences; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
