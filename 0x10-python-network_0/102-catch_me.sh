2a333a4f1b5c82eff9977f6307970267d967ee6f
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-04-25 16:35:00 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me -o /dev/null
-
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
