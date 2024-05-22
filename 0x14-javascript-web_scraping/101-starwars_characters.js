4855e7af9e08f19c6c88695b6f9476642fdb75e7
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-05-22 04:49:11 +0300

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,41 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+    return;
+  }
+
+  try {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+
+    characters.forEach(characterUrl => {
+      request.get(characterUrl, (error, response, body) => {
         if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
+          console.error('Error:', error);
+          return;
         }
+
+        if (response.statusCode !== 200) {
+          console.error('Status:', response.statusCode);
+          return;
+        }
+
+        const characterData = JSON.parse(body);
+        console.log(characterData.name);
       });
-    }
+    });
+  } catch (parseError) {
+    console.error('Parse error:', parseError);
   }
 });
